1773e3676b7ef9814c0b1988634cd179ea3fe489
GhDamghani <130757093+GhDamghani@users.noreply.github.com>
2023-05-06 00:44:24 +0200

    Update according to new code

--- examples/02 - 3D Flow bifurcation diagram/run.py
+++ examples/02 - 3D Flow bifurcation diagram/run.py
@@ -3,14 +3,14 @@ import matplotlib.pyplot as plt
 from numpy import linspace
 from julia import Main
 
-f = Main.eval(open('func.jl', 'r').read())
+f = Main.eval(open('ES1.jl', 'r').read())
 
 sys1 = qand.DiffEq(f, 3)
 u0 = [6., 0., -1.0]
-tspan = (0., 3000.)
+tspan = (0., 2000.)
 p = [1.54]
 n = 500
-p_array = linspace(1.52, 1.5538, n, 1)
+p_array = linspace(1.52, 1.56, n, 1)
 
 from os import listdir
 # Check if the bifurcation has already been saved so we don't have to compute it again.
@@ -19,7 +19,7 @@ bif_file = 'Bifurcation.qand'
 current_files = listdir()
 if bif_file not in current_files:
     # Computing trajectory
-    bif = sys1.bifurcation(0, p, p_array, u0, tspan, mode='min', transient=100, u0_forward=True, tqdm=True, num_points=500)
+    bif = sys1.bifurcation(0, p, p_array, u0, tspan, mode='min', transient=30, u0_forward=True, tqdm=True, num_points=100, print_max_num_points=True)
     bif.save(bif_file)
 else:
     print('Already computed, loading...')
