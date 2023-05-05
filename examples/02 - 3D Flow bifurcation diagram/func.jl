8b1097881314cf4ddf95b73d8a0ff8ac8dce441d
GhDamghani <130757093+GhDamghani@users.noreply.github.com>
2023-05-06 00:44:15 +0200

    Delete

--- examples/02 - 3D Flow bifurcation diagram/func.jl
+++
@@ -1,9 +0,0 @@
-# Jafari, Sajad, et al. "Simple chaotic 3D flows with surfaces of equilibria." Nonlinear Dynamics 86 (2016): 1349-1358.
-function ES1!(du, u, p, t)
-  x, y, z = u
-  a = p[1]
-  f = x
-  du[1] = f*y
-  du[2] = f*z
-  du[3] = f*(-x+a*y^2-x*z)
-end
